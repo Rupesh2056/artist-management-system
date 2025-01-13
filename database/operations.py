7bec81c922638e2fff9a45f3d8812860990d8aec
Rupesh2056 <rrrupesh.chaulagain@gmail.com>
2025-01-13 18:03:49 +0545

    type hints added for db operations

--- database/operations.py
+++ database/operations.py
@@ -34,38 +34,54 @@ user_select_fields = ""
 from django.db import connection
 import os
 import django
+from typing import Any
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 # django.setup()
 
-def execute_select_query(query,filters=None):
+def execute_select_query(query,filters=None) -> list[tuple]:
+        '''
+        Executes Database query and returns List of tuples.
+        '''
         with connection.cursor() as cursor:
                 cursor.execute(query) if not filters else cursor.execute(query,filters)
                 rows = cursor.fetchall()
         return rows
 
-def execute_select_first_query(query,filters=None):
+def execute_select_first_query(query,filters=None) -> tuple:
         with connection.cursor() as cursor:
                 cursor.execute(query) if not filters else cursor.execute(query,filters)
                 row = cursor.fetchone()
         return row
 
-def execute_insert_query(query,values):
+def execute_insert_query(query,values) -> None:
+        ''''
+        Executes Insert query to database.
+        '''
         with connection.cursor() as cursor:
                 cursor.execute(query,values)
 
 
-def execute_create_table_query(query):
+def execute_create_table_query(query) -> None:
+        ''''
+        Executes Create Table query to database.
+        '''
         with connection.cursor() as cursor:
                 cursor.execute(query)
-                # conn.commit()
 
-def execute_create_enum_query(query):
+
+def execute_create_enum_query(query) -> None:
+       ''''
+        Executes Create Enum query to database.
+        '''
        with connection.cursor() as cursor:
                 cursor.execute(query)
 
-def check_if_exists(query,values):
+def check_if_exists(query,values) -> Any[tuple,None]:
+        '''
+        Returns either Tuple or None
+        '''
         with connection.cursor() as cursor:
                 cursor.execute (query,values)
                 return cursor.fetchone()[0]
